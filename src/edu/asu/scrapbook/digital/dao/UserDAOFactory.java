412d2ec4237048c687bb147f5c1a6a9ff6db25fa
mcmathews <mcmathe1@asu.edu>
2014-10-22 13:37:11 -0700

    Fix errors commited earlier by added getInstance() to UserDAOFactory

--- src/edu/asu/scrapbook/digital/dao/UserDAOFactory.java
+++ src/edu/asu/scrapbook/digital/dao/UserDAOFactory.java
@@ -1,5 +1,7 @@
 package edu.asu.scrapbook.digital.dao;
 
 public class UserDAOFactory {
-	
+	public static UserDAO getInstance() {
+		return new UserDAOImpl();
+	}
 }
--- src/edu/asu/scrapbook/digital/rest/UserResource.java
+++ src/edu/asu/scrapbook/digital/rest/UserResource.java
@@ -1,11 +1,13 @@
 package edu.asu.scrapbook.digital.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import edu.asu.scrapbook.digital.dao.UserDAO;
 import edu.asu.scrapbook.digital.dao.UserDAOFactory;
 import edu.asu.scrapbook.digital.model.User;
 
@@ -18,6 +20,13 @@ public class UserResource {
 	public User getOne(@PathParam("username") String username) {
 		if (username != null && !username.isEmpty()) {
 			UserDAO dao = UserDAOFactory.getInstance();
+			try {
+				return dao.findById();
+			} catch (Exception e) {
+				return null;
+			}
+		} else {
+			throw new NotFoundException();
 		}
 	}
 }
