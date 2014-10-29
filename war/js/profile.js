7f75da0681f7a5a627e1f50ca02dde12f55c3176
mcmathews <mcmathe1@asu.edu>
2014-10-29 13:17:16 -0700

    Add default profile image

--- war/js/profile.js
+++ war/js/profile.js
@@ -4,6 +4,12 @@ $(function () {
 		type: "GET",
 		dataType: "json",
 		success: function (data, status, jqxhr) {
+			if (!data.settings.profileImage) {
+				data.settings.profileImage = {
+						datastoreLink: "https://gp5.googleusercontent.com/-ccHrCOypIOk/AAAAAAAAAAI/AAAAAAAAAAA/W4izi6XQshc/s48-c-k-no/photo.jpg",
+						title: "No profile image"
+				};
+			}
 			ko.applyBindings(data);
 		}
 	});
