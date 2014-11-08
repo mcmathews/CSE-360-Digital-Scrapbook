b2c59308cbc212d9228678f3c14f7569bb60420c
mcmathews <mcmathe1@asu.edu>
2014-11-08 13:50:50 -0700

    Implemented tests for UserUtil.userExists

--- src/edu/asu/scrapbook/digital/test/UserUtilTest.java
+++ src/edu/asu/scrapbook/digital/test/UserUtilTest.java
@@ -1,14 +1,21 @@
 package edu.asu.scrapbook.digital.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.VoidWork;
 
+import edu.asu.scrapbook.digital.dao.UserDAO;
+import edu.asu.scrapbook.digital.dao.UserDAOFactory;
+import edu.asu.scrapbook.digital.model.User;
 import edu.asu.scrapbook.digital.util.UserUtil;
 
 public class UserUtilTest {
@@ -36,7 +43,43 @@ public class UserUtilTest {
 	
 	@Test
 	public void testUserExists() {
-		fail("Not yet implemented");
+		ObjectifyService.run(new VoidWork() {
+		    public void vrun() {
+		    	LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+				helper.setUp();
+				
+				User user = new User();
+				user.setUsername("test@test.com");
+				
+				UserDAO userDAO = UserDAOFactory.getInstance();
+				try {
+					userDAO.create(user);
+					assertTrue("userExists() returned false negative", UserUtil.userExists("test@test.com"));
+				} catch (Exception e) {
+					throw new RuntimeException("UserDAO failed: " + e);
+				}
+				
+				helper.tearDown();
+		    }
+		});
+	}
+	
+	@Test
+	public void testUserDoesNotExists() {
+		ObjectifyService.run(new VoidWork() {
+		    public void vrun() {
+		    	LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+				helper.setUp();
+				
+				try {
+					assertFalse("userExists() returned false positive", UserUtil.userExists("test@test.com"));
+				} catch (Exception e) {
+					throw new RuntimeException("UserDAO failed: " + e);
+				}
+				
+				helper.tearDown();
+		    }
+		});
 	}
 	
 }
