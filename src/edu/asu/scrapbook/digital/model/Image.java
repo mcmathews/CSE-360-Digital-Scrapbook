3968018e77e72c3fe534f9d28fb636ea1b5498e2
mcmathews <mcmathe1@asu.edu>
2014-11-08 13:35:50 -0700

    Override equals() and hashCode() for pojos

--- src/edu/asu/scrapbook/digital/model/Image.java
+++ src/edu/asu/scrapbook/digital/model/Image.java
@@ -7,7 +7,8 @@ import com.googlecode.objectify.annotation.Id;
 @Entity
 public class Image {
 	
-	@Id private Long id;
+	@Id
+	private Long id;
 	private String title;
 	private String filename;
 	private String datastoreLink;
@@ -36,20 +37,69 @@ public class Image {
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
-
+	
 	public String getDatastoreLink() {
 		return datastoreLink;
 	}
-
+	
 	public void setDatastoreLink(String datastoreLink) {
 		this.datastoreLink = datastoreLink;
 	}
-
+	
 	public BlobKey getBlobKey() {
 		return blobKey;
 	}
-
+	
 	public void setBlobKey(BlobKey blobKey) {
 		this.blobKey = blobKey;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((blobKey == null) ? 0 : blobKey.hashCode());
+		result = prime * result + ((datastoreLink == null) ? 0 : datastoreLink.hashCode());
+		result = prime * result + ((filename == null) ? 0 : filename.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Image))
+			return false;
+		Image other = (Image) obj;
+		if (blobKey == null) {
+			if (other.blobKey != null)
+				return false;
+		} else if (!blobKey.equals(other.blobKey))
+			return false;
+		if (datastoreLink == null) {
+			if (other.datastoreLink != null)
+				return false;
+		} else if (!datastoreLink.equals(other.datastoreLink))
+			return false;
+		if (filename == null) {
+			if (other.filename != null)
+				return false;
+		} else if (!filename.equals(other.filename))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
 }
--- src/edu/asu/scrapbook/digital/model/User.java
+++ src/edu/asu/scrapbook/digital/model/User.java
@@ -10,13 +10,15 @@ import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class User {
-	@Id private String username;
+	@Id
+	private String username;
 	private ProfileSettings settings;
 	
 	@JsonIgnore
 	private List<Ref<Image>> images = new ArrayList<>();
 	
-	public User() {}
+	public User() {
+	}
 	
 	public String getUsername() {
 		return username;
@@ -33,13 +35,43 @@ public class User {
 	public void setSettings(ProfileSettings settings) {
 		this.settings = settings;
 	}
-
+	
 	public List<Ref<Image>> getImages() {
 		return images;
 	}
-
+	
 	public void setImages(List<Ref<Image>> images) {
 		this.images = images;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((settings == null) ? 0 : settings.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (settings == null) {
+			if (other.settings != null)
+				return false;
+		} else if (!settings.equals(other.settings))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 }
