a37ec50c1cd4a32f610699a72026ea55f026f154
ejmarsh1 <ejmarsh1@asu.edu>
2014-11-08 23:26:00 -0700

    Added the DatastoreImageTest class to the SuiteClasses annotation

--- src/edu/asu/scrapbook/digital/test/AllTests.java
+++ src/edu/asu/scrapbook/digital/test/AllTests.java
@@ -5,5 +5,5 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DigitalScrapbookUnitTest.class, DatastoreUserTest.class, ImageResourceTest.class, UserResourceTest.class, UserUtilTest.class })
+@SuiteClasses({ DigitalScrapbookUnitTest.class, DatastoreUserTest.class, ImageResourceTest.class, UserResourceTest.class, UserUtilTest.class, DatastoreImageTest.class })
 public class AllTests {}
