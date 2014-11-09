d42ba53ecc7baf0318dd8e2d23abfeaa2403de32
mcmathews <mcmathe1@asu.edu>
2014-11-08 22:53:20 -0700

    Added other test classes to AllTests suite

--- src/edu/asu/scrapbook/digital/test/AllTests.java
+++ src/edu/asu/scrapbook/digital/test/AllTests.java
@@ -5,5 +5,5 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DigitalScrapbookUnitTest.class})
+@SuiteClasses({ DigitalScrapbookUnitTest.class, DatastoreUserTest.class, ImageResourceTest.class, UserResourceTest.class, UserUtilTest.class })
 public class AllTests {}
